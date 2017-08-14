bff4a6bd1fe401270f2b54ec58a6ac789e7d61ac
godAndrew <wedolovechemistry@gmail.com>
2017-08-14 02:24:35 -0300

    Facebook Auth updated, now with 2 activities!

--- LoginActivity.kt
+++ LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.user.facebookauth2
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
@@ -11,34 +12,49 @@ import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import kotlinx.android.synthetic.main.activity_login.*
 import com.facebook.FacebookException
+import java.util.*
 
 
-class LoginActivity : AppCompatActivity() {
-
-
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var callbackManager : CallbackManager
+
+
+    override fun onClick(v: View?) {
+        when (v){
+            login_face ->{
+                loginWithFb()
+            }
+        }
+    }
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FacebookSdk.sdkInitialize(applicationContext)
         setContentView(R.layout.activity_login)
+
         initializeControls()
-        loginWithFb()
+        //loginWithFb()
     }
 
 //LoginManager.getInstance() //face_login_button
     private fun loginWithFb(){
-
+        //LoginManager.getInstance().logInWithPublishPermissions(this,Arrays.asList("publish_actions"))
+        LoginManager.getInstance().logInWithReadPermissions(this,
+                Arrays.asList("email","public_profile"/*"user_events"*/))
         LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
 
             override fun onSuccess(result: LoginResult) {
                 //textStatus.text = "Login Succesfull.\n" + result.accessToken
                 goMainScream()
+
             }
 
             override fun onCancel() {
-                //textStatus.text = "Login Cancelled."
+                textStatus.text = "Login Cancelled."
                 Toast.makeText(applicationContext,"Login Cancelled", Toast.LENGTH_SHORT).show()
 
             }
@@ -57,6 +73,7 @@ class LoginActivity : AppCompatActivity() {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or
                 Intent.FLAG_ACTIVITY_CLEAR_TASK or
                 Intent.FLAG_ACTIVITY_NEW_TASK)
+
         startActivity(intent)
     }
 
@@ -69,6 +86,8 @@ class LoginActivity : AppCompatActivity() {
 
 
     private fun initializeControls(){
+        //face_login_button.setPublishPermissions(Arrays.asList("email","public_profile", "user_friends"))
+        login_face.setOnClickListener(this)
         callbackManager = CallbackManager.Factory.create()
     }
 
--- MainActivity.kt
+++ MainActivity.kt
@@ -1,42 +1,69 @@
 package com.example.user.facebookauth2
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.facebook.AccessToken
+import com.facebook.Profile
 import com.facebook.login.LoginManager
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var profile: Profile
+
+
     override fun onClick(v: View?) {
         when (v) {
-            button_logOut ->{
+            button_logOut -> {
                 LoginManager.getInstance().logOut()
                 goLoginScream()
             }
         }
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(AccessToken.getCurrentAccessToken() == null){
+        //LoginManager.getInstance().logOut()// Caso suas permissoes buguem desconecte o usuario aqui depois comente essa linha
+        if (AccessToken.getCurrentAccessToken() == null) {
             goLoginScream()
         }
-        button_logOut.setOnClickListener(this)
-        textStatus2.text = "Login Succesfull.\n" + AccessToken.getCurrentAccessToken()
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (AccessToken.getCurrentAccessToken() != null) {
+            profile = Profile.getCurrentProfile()
+            button_logOut.setOnClickListener(this)
+            text_Infos.text = profile.firstName + " " + profile.lastName
+            carregarImagem()
+
+
+        }
     }
 
 
+    private fun carregarImagem() {
+        profile.getProfilePictureUri(200, 200)
+
+        Picasso.with(img_perfil.context).load(profile.getProfilePictureUri(200, 200)).into(img_perfil)
+
+    }
+
     private fun goLoginScream() {
-        val intent : Intent = Intent(this,LoginActivity::class.java)
+
+        val intent: Intent = Intent(this, LoginActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or
                 Intent.FLAG_ACTIVITY_CLEAR_TASK or
                 Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
     }
-
 
 
 }
